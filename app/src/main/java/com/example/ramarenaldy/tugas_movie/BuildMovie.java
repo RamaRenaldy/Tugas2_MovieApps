556ad628b76f00476ea5d0109b5a017db2f2bc5b
RamaRenaldy <rrenaldy29@gmail.com>
2018-10-19 18:29:20 +0700

    Tugas2_MovieApps

---
+++ app/src/main/java/com/example/ramarenaldy/tugas_movie/BuildMovie.java
@@ -0,0 +1,12 @@
+package com.example.ramarenaldy.tugas_movie;
+
+public class BuildMovie {
+    String title;
+    String rating;
+    String status;
+    public BuildMovie(String ltitle, String lrating, String lstatus) {
+        this.title = ltitle;
+        this.rating = lrating;
+        this.status = lstatus;
+    }
+}
